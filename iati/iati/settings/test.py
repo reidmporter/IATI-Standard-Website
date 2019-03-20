5e8595d518676b2e2a6d4115519f0ec6829dc147
Francesca Sargent <francesca@fffunction.co>
2019-03-20 09:41:00 +0000

    Adding travis IPs for tests

--- iati/iati/settings/test.py
+++ iati/iati/settings/test.py
@@ -2,7 +2,21 @@
 import os
 from .base import *  # noqa: F401, F403 # pylint: disable=unused-wildcard-import, wildcard-import
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "35.184.96.71",
+    "35.184.226.236",
+    "35.188.1.99",
+    "35.188.73.34",
+    "35.192.85.2",
+    "35.192.136.167",
+    "35.192.187.174",
+    "35.193.7.13",
+    "35.202.145.110",
+    "35.224.112.202",
+    "104.154.113.151",
+    "104.154.120.187",
+    "localhost",
+]
 
 SECRET_KEY = '-sg0o=f6(j3!4u6^86!j@0&l^3clslh-#f@02d2^p_4vy0ma0y'
 
