d2def96a039ade10198e4231ca2e0663cd172315
Francesca Sargent <francesca.sargent@devinit.org>
2019-03-25 09:21:13 +0000

    Adding unit test for createdefaultpages management command

--- iati/home/tests/test_management.py
+++ iati/home/tests/test_management.py
@@ -3,6 +3,8 @@ import pytest
 import requests
 from io import StringIO
 from django.core.management import call_command
+from wagtail.core.models import Page
+from home.management.commands.createdefaultpages import DEFAULT_PAGES
 from home.management.commands.updatestatistics import (
     ACTIVITY_URL,
     ORGANISATION_URL,
@@ -37,3 +39,21 @@ class TestUpdateStatistics():
 
         assert out_value == success_message
         assert out_value not in error_messages
+
+
+@pytest.mark.django_db
+class TestCreateDefaultPages():
+    """Unit test to test createdefaultpages management command."""
+
+    def test_create_default_pages_command(self):
+        """Test result for called command returns expected response."""
+        out = StringIO()
+        call_command('createdefaultpages', stdout=out)
+        success_message = 'Success.'
+        out_value = out.getvalue().rstrip()
+
+        expected_pages = [x['title'] for x in DEFAULT_PAGES]
+        qs = Page.objects.filter(title__in=expected_pages).all()
+
+        assert success_message in out_value
+        assert len(qs) > 0
