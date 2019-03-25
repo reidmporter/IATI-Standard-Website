4eca6e4a8a747443eb94c7c4d599ff89ed15af59
Francesca Sargent <francesca.sargent@devinit.org>
2019-03-25 09:29:02 +0000

    Amending tests for home page models

--- iati/home/tests/test_models.py
+++ iati/home/tests/test_models.py
@@ -3,6 +3,22 @@ from about.factories import CaseStudyPageFactory, CaseStudyIndexPageFactory
 from home.models import HomePage
 
 
+@pytest.fixture()
+def case_studies():
+    """Fixture to create batched EventPage tree."""
+    home_page = HomePage.objects.first()
+    case_study_index = CaseStudyIndexPageFactory(
+        title='Case Studies',
+        parent=home_page
+    )
+    CaseStudyPageFactory.create_batch(
+        size=20,
+        parent=case_study_index
+    )
+
+    return case_study_index
+
+
 @pytest.mark.django_db
 class TestHomePage():
     """Tests for Home Page."""
@@ -12,35 +28,23 @@ class TestHomePage():
         """Return HomePage created in migrations."""
         return HomePage.objects.first()
 
-    def test_home_page_lists_all_case_studies(self, client):
-        """Test that the homepage title is as set by the factory."""
-        case_study_index = CaseStudyIndexPageFactory(
-            title='Case Studies',
-            parent=self.home_page
-        )
-        case_study_pages = CaseStudyPageFactory.create_batch(
-            size=10,
-            parent=case_study_index
-        )
-
+    def test_home_page_lists_all_case_studies(self, client, case_studies):
+        """Test that case studies are passed to HomePage context."""
         home_page_context = client.get(self.home_page.url, follow=True).context
         home_page_case_studies = home_page_context.get('case_studies', None)
+        case_study_pages = case_studies.get_children()
 
         assert len(case_study_pages) == len(home_page_case_studies)
 
-    def test_home_page_lists_live_case_studies(self, client):
-        """Test that the homepage title is as set by the factory."""
-        case_study_index = CaseStudyIndexPageFactory(
-            title='Case Studies',
-            parent=self.home_page
-        )
+    def test_home_page_lists_live_case_studies(self, client, case_studies):
+        """Test that only live case studies are passed to context."""
         CaseStudyPageFactory.create_batch(
             size=10,
-            parent=case_study_index,
+            parent=case_studies,
             live=False,
         )
 
         home_page_context = client.get(self.home_page.url, follow=True).context
         home_page_case_studies = home_page_context.get('case_studies', None)
 
-        assert len(home_page_case_studies) == 0
+        assert len(home_page_case_studies) == 20
